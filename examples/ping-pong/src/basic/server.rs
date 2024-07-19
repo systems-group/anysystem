7e23a89fa01ac70de0656d614c948dada3a5aff5
Oleg Sukhoroslov <oleg.sukhoroslov@gmail.com>
2024-07-19 22:21:18 +0300

    Rename to AnySystem

---
+++ examples/ping-pong/src/basic/server.rs
@@ -0,0 +1,24 @@
+use anysystem::context::Context;
+use anysystem::message::Message;
+use anysystem::process::Process;
+
+#[derive(Clone)]
+pub struct BasicPingServer {}
+
+impl Process for BasicPingServer {
+    fn on_message(&mut self, msg: Message, from: String, ctx: &mut Context) -> Result<(), String> {
+        if msg.tip == "PING" {
+            let resp = Message::new("PONG".to_string(), msg.data);
+            ctx.send(resp, from);
+        }
+        Ok(())
+    }
+
+    fn on_local_message(&mut self, _msg: Message, _ctx: &mut Context) -> Result<(), String> {
+        Ok(())
+    }
+
+    fn on_timer(&mut self, _timer: String, _ctx: &mut Context) -> Result<(), String> {
+        Ok(())
+    }
+}
