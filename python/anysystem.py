3fee2442fa79976f9e1c244da13de6e0d45823c6
Alexander Shirokovskikh <alex.shir.141@gmail.com>
2025-12-22 20:32:27 +0300

    Small refactor

--- python/anysystem.py
+++ python/anysystem.py
@@ -17,20 +17,20 @@ class Message:
     def type(self) -> str:
         return self._type
 
+    def get(self, key: str, default: Any = None) -> Any:
+        return self._data.get(key, default)
+
+    def remove(self, key: str):
+        self._data.pop(key, None)
+
+    def __contains__(self, key: str) -> bool:
+        return key in self._data
+
     def __getitem__(self, key: str) -> Any:
         return self._data[key]
 
     def __setitem__(self, key: str, value: Any):
         self._data[key] = value
-
-    def remove(self, key: str):
-        self._data.pop(key, None)
-
-    def get(self, key: str, default: Any = None) -> Any:
-        return self._data.get(key, default)
-
-    def __contains__(self, key: str) -> bool:
-        return key in self._data
 
     @staticmethod
     def from_json(message_type: str, json_str: str) -> Message:
